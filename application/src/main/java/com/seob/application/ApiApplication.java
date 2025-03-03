e32e9d03271afda39c12a8fd19a3916dfd4137a3
LeeHyeonseob <hyeonseob22@gmail.com>
2025-03-04 00:47:44 +0900

    chore: 모듈 구성 수정

--- application/src/main/java/com/seob/application/ApiApplication.java
+++ application/src/main/java/com/seob/application/ApiApplication.java
@@ -2,8 +2,14 @@ package com.seob.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = "com.seob"
+)
+@EnableJpaRepositories(basePackages = "com.seob.systeminfra")
+@EntityScan(basePackages = "com.seob.systeminfra")
 public class ApiApplication {
 
 	public static void main(String[] args) {
