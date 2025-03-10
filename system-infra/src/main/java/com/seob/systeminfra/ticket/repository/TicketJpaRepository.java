5c6d03449b4eaf56134cf4c384cced20a3341344
LeeHyeonseob <hyeonseob22@gmail.com>
2025-03-10 13:49:10 +0900

    fix: TicketJpaRepository TicketId String 반영

--- system-infra/src/main/java/com/seob/systeminfra/ticket/repository/TicketJpaRepository.java
+++ system-infra/src/main/java/com/seob/systeminfra/ticket/repository/TicketJpaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TicketJpaRepository extends JpaRepository<TicketEntity, Long> {
+public interface TicketJpaRepository extends JpaRepository<TicketEntity, String> {
     Optional<TicketEntity> findByUserId(String userId);
 
     Optional<TicketEntity> findById(String id);
