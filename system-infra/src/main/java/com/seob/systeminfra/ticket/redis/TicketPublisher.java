bdf81a263303e435f8f32e2302adbbd526ded63a
LeeHyeonseob <hyeonseob22@gmail.com>
2025-03-14 15:39:03 +0900

    refactor: 전환과정 메서드 분리 리팩토링

--- system-infra/src/main/java/com/seob/systeminfra/ticket/redis/TicketPublisher.java
+++ system-infra/src/main/java/com/seob/systeminfra/ticket/redis/TicketPublisher.java
@@ -25,16 +25,21 @@ public class TicketPublisher {
 
     public void publish(TicketDomain ticketDomain) {
         // 도메인 객체에서 필요한 데이터만 맵으로 추출
-        Map<String, String> ticketData = new HashMap<>();
-        ticketData.put("ticketId", ticketDomain.getId().getValue());
-        ticketData.put("userId", ticketDomain.getUserId().getValue());
-        ticketData.put("createdAt", ticketDomain.getCreatedAt().toString()); // ISO 형식으로 문자열 변환
-        ticketData.put("isUsed", String.valueOf(ticketDomain.isUsed()));
+        Map<String, String> ticketData = extractTicketData(ticketDomain);
 
 
         // MapRecord 생성 및 스트림에 추가
         MapRecord<String, String, String> record = MapRecord.create(streamKey, ticketData);
         redisTemplate.opsForStream().add(record);
+    }
+
+    private Map<String, String> extractTicketData(TicketDomain ticketDomain) {
+        Map<String, String> ticketData = new HashMap<>();
+        ticketData.put("ticketId", ticketDomain.getId().getValue());
+        ticketData.put("userId", ticketDomain.getUserId().getValue());
+        ticketData.put("createdAt", ticketDomain.getCreatedAt().toString()); // ISO 형식으로 문자열 변환
+        ticketData.put("isUsed", String.valueOf(ticketDomain.isUsed()));
+        return ticketData;
     }
 
 
