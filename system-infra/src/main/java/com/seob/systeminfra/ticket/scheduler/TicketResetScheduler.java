e25c26f47734069888c656ec0d125c7f1d28a324
LeeHyeonseob <hyeonseob22@gmail.com>
2025-03-15 15:10:24 +0900

    feat: 티켓 발급 중복 체크 및 티켓 redis 초기화 스케줄러 구현

---
+++ system-infra/src/main/java/com/seob/systeminfra/ticket/scheduler/TicketResetScheduler.java
@@ -0,0 +1,31 @@
+package com.seob.systeminfra.ticket.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+@Slf4j
+public class TicketResetScheduler {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Value("${redis.ticket.issued-set}")
+    private String ticketIssuedSet;
+
+    @Value("${redis.ticket.counter-key}")
+    private String ticketCounterKey;
+
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void resetTicketSystem() {
+        redisTemplate.delete(ticketIssuedSet);
+        redisTemplate.opsForValue().set(ticketCounterKey, 0);
+        log.info("티켓 정보 초기화");
+    }
+}
