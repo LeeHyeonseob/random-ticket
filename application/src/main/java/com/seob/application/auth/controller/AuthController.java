bc87eefc63378f69868a5e74cd104dce4c3b72c0
LeeHyeonseob <hyeonseob22@gmail.com>
2025-05-16 00:52:58 +0900

    fix: 이메일 검증 api patch로 변경

--- application/src/main/java/com/seob/application/auth/controller/AuthController.java
+++ application/src/main/java/com/seob/application/auth/controller/AuthController.java
@@ -18,10 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -62,7 +59,7 @@ public class AuthController {
             @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
         }
     )
-    @PostMapping("/verify")
+    @PatchMapping("/verification")
     public ResponseEntity<VerificationResponse> verifyEmail(@RequestBody VerifyEmailRequest request){
         VerificationServiceResponse serviceResponse = authService.verifyEmail(
                 request.email(),
